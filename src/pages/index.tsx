908ac8bed4a5e073e2215721d26b0bfcd87c701c
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2023-03-03 17:50:57 -0300

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Root } from "../components/root";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Root />
+    </div>
+  );
+};
+
+export default Home;
